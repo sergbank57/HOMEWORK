b1145533cf3d749a0d5b5db5ef3f6b3d0ea98fa8
denis <79594203sa@gmail.com>
2023-01-10 00:57:16 +0100

    test

---
+++ src/HW_1_JavaBasicLec1/task_3_multitable.java
@@ -0,0 +1,9 @@
+package HW_1_JavaBasicLec1;
+
+public class task_3_multitable {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        //  System - с заглавной буквы и не забываем про ";" в конце строк
+        System.out.println("123");
+    }
+}
