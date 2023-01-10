adcefe0f584d2cb50b69c1692a35c75e2eb43760
denis <79594203sa@gmail.com>
2023-01-10 15:33:50 +0100

    testic

---
+++ src/HW_1_JavaBasicLec1/Calculator.java
@@ -0,0 +1,24 @@
+package HW_1_JavaBasicLec1;
+
+public class Calculator {
+    public double sum(double x, double y) {
+        double resultsum = x + y;
+        return resultsum;
+    }
+
+    public double subm(double x, double y) {
+        double resultsubm = x - y;
+        return resultsubm;
+    }
+
+    public double multip(double x, double y) {
+        double resultmultip = x * y;
+        return resultmultip;
+    }
+
+    public double divis(double x, double y) {
+        double resultdivis = x / y;
+        return resultdivis;
+    }
+}
+
--- src/HW_1_JavaBasicLec1/task_1_DoubleNumber.java
+++ src/HW_1_JavaBasicLec1/task_1_DoubleNumber.java
@@ -4,13 +4,21 @@ import java.util.Scanner;
 
 public class task_1_DoubleNumber {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
         System.out.println("введите два числа");
 
         double firstDoubleNumber = scanner.nextDouble();
         double secondDoubleNumber = scanner.nextDouble();
         String name = scanner.nextLine();
-        System.out.println("Hello " + name);
+      double resaultsum =  calculator.sum(firstDoubleNumber,secondDoubleNumber);
+      System.out.println("Resaul sum = " + resaultsum);
+        double resaultsubm =  calculator.subm(firstDoubleNumber,secondDoubleNumber);
+        System.out.println("Resaul subm = " + resaultsubm);
+       double resaultmultip =  calculator.multip(firstDoubleNumber,secondDoubleNumber);
+        System.out.println("Resaul multiplic = " + resaultmultip);
+        double resaultdivis =  calculator.divis(firstDoubleNumber,secondDoubleNumber);
+        System.out.println("Resaul divis = " + resaultdivis);
     }
 }
 
